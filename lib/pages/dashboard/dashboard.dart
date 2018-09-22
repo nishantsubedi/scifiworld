7866d70db449fc4c4131535f9ed70e2c9e76c41a
Nishant <nishantsubedi1996@gmail.com>
2018-09-22 12:31:24 +0545

    navigation added

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:scifiworld/pages/dashboard/widgets/navigationBar.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -12,6 +11,8 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  String activeButton;
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -19,17 +20,98 @@ class _DashboardState extends State<Dashboard> {
         _backButtonClicked();
       },
       child: Scaffold(
-        body:  Container(
-          padding: EdgeInsets.only(top:24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                NavigationBar()
-              ],
-            ),
-          )
-        )
-      ),
+          body: Container(
+              padding: EdgeInsets.only(top: 24.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      color: Theme.of(context).primaryColorDark,
+                      height: 60.0,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.home,
+                              color: activeButton == 'home'
+                                  ? Colors.white
+                                  : Colors.white70,
+                            ),
+                            iconSize: 40.0,
+                            onPressed: (){
+                              setState(() {
+                                activeButton = 'home';
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          FlatButton(
+                              child: Opacity(
+                            opacity: 0.9,
+                            child: Image.asset(
+                              "images/news_icon.png",
+                              color: activeButton == 'news'
+                                  ? Colors.white
+                                  : Colors.white70,
+                              height: 40.0,
+                              width: 40.0,
+                            ),
+                          ),
+                          onPressed: (){
+                            setState(() {
+                                activeButton = 'news';
+                              });
+                          },),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.notifications,
+                              color: activeButton == 'notification'
+                                  ? Colors.white
+                                  : Colors.white70,
+                            ),
+                            iconSize: 40.0,
+                            onPressed: (){
+                              setState(() {
+                                activeButton = 'notification';
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.dehaze,
+                              color: activeButton == 'control'
+                                  ? Colors.white
+                                  : Colors.white70,
+                            ),
+                            iconSize: 40.0,
+                            onPressed: () {
+                              setState(() {
+                                activeButton = 'control';
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ))),
     );
   }
 
@@ -48,13 +130,23 @@ class _DashboardState extends State<Dashboard> {
           ),
           actions: <Widget>[
             new FlatButton(
-              child: new Text('Okay'),
+              child: new Text(
+                'Okay',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColorDark,
+                ),
+              ),
               onPressed: () {
                 exit(0);
               },
             ),
             new FlatButton(
-              child: new Text('Cancel'),
+              child: new Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColorDark,
+                ),
+              ),
               onPressed: () {
                 Navigator.of(context).pop();
               },
