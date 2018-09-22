4c38b23cabe91dbc43b4bbfdec671cbc72d2afea
Nishant <nishantsubedi1996@gmail.com>
2018-09-22 14:55:53 +0545

    items added on homepage

--- lib/pages/homepage/homePage.dart
+++ lib/pages/homepage/homePage.dart
@@ -2,14 +2,63 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return _HomePageState();
-    }
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
 }
 
 class _HomePageState extends State<HomePage> {
   @override
-    Widget build(BuildContext context) {
-      return Text('Home');
-    }
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              color: Theme.of(context).primaryColor,
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: ListTile(
+                leading: Icon(Icons.theaters, color: Colors.white, size: 30.0,),
+                title: Text('Movies', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+              ),
+            ),
+            Container(
+              color: Theme.of(context).primaryColor,
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: ListTile(
+                leading: Icon(Icons.live_tv, color: Colors.white, size: 30.0,),
+                title: Text('TV Shows', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 10.0),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              color: Theme.of(context).primaryColor,
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: ListTile(
+                leading: Icon(Icons.book, color: Colors.white, size: 30.0,),
+                title: Text('Books and Comics', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+              ),
+            ),
+            Container(
+              color: Theme.of(context).primaryColor,
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: ListTile(
+                leading: Icon(Icons.explore, color: Colors.white, size: 30.0,),
+                title: Text('Technology', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
 }
